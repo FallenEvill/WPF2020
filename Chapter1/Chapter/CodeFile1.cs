17503430d64a993f451b836a16ada26786624b84
FallenEvill <50545892+FallenEvill@users.noreply.github.com>
2020-03-02 17:28:23 +0500

    Add files via upload

---
+++ Chapter1/Chapter/CodeFile1.cs
@@ -0,0 +1,18 @@
+﻿//----------------------------------------- // SayHello.cs (c) 2006 by Charles Petzold //----------------------------------------- 
+using System; 
+using System.Windows; 
+namespace Petzold.SayHello 
+{     
+    class SayHello     // Название класса //
+    {         
+        [STAThread]         
+        public static void Main()         
+        {             
+            Window win = new Window(); // Создание нового окна //            
+            win.Title = "Say Hello";     // Заголовок окна //        
+            win.Show(); // Открытие окна //      
+            Application app = new Application();     // Создание приложения // 
+            app.Run(); // Запуск приложения // 
+        }     
+    } 
+} 
