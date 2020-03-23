df5d212a39be2f6ee3c4e502c797a662c2bde584
FallenEvill <50545892+FallenEvill@users.noreply.github.com>
2020-03-23 17:27:01 +0500

    Add files via upload

--- Movement/Movement/Program.cs
+++ Movement/Movement/Program.cs
@@ -13,8 +13,10 @@ namespace Movement
         private double xSpd;
         private double ySpd;
         private double angle;
+        private double m = 1;
         private const double g = 9.8;
         private List<List<double>> cXYT = new List<List<double>>();
+        private List<double> wind = new List<double>();
 
         public Move(double s, double a)
         {
@@ -30,33 +32,48 @@ namespace Movement
         }
         public void Calculate(double s)
         {
+            double x = 0, y = 0;
             StreamWriter f = new StreamWriter("test123.txt");
-            double flt = 2 * ySpd / g;
-            for (double i = 1; i < flt; i += s)
+            for (double i = s; true; i += s)
             {
                 InitList();
-                cXYT[0].Add(xSpd * i);
-                cXYT[1].Add(ySpd * i - (g * i * i / 2));
+                xSpd = xSpd -xSpd * WindX(i) * i / m;
+                ySpd = ySpd + (-ySpd * WindY(i) / m -g) * i;
+                x = x + xSpd * i;
+                y = y + ySpd * i;
+                if (y < 0)
+                    break;
+                cXYT[0].Add(x);
+                cXYT[1].Add(y);
                 cXYT[2].Add(i);
-                for (int j = 0; j < 3; j++)
+
+                    f.WriteLine("{0} {1} {2}",cXYT[0].Last(), cXYT[1].Last(), cXYT[2].Last());
+                    Console.WriteLine("{0} {1} {2}", cXYT[0].Last(), cXYT[1].Last(), cXYT[2].Last());
+                if (y < 0)
                 {
-                    f.WriteLine(cXYT[j].Last());
-                    Console.WriteLine(cXYT[j].Last());
-                }
-                if ((flt - i) < s)
-                {
-                    cXYT[0].Add(xSpd * flt);
+
+                    cXYT[0].Add(x);
                     cXYT[1].Add(0);
-                    cXYT[2].Add(flt);
+                    cXYT[2].Add(i);
                     for (int j = 0; j < 3; j++)
                     {
                         f.WriteLine(cXYT[j].Last());
                     }
                     f.Close();
+                    break;
                 }
             }
-            Console.WriteLine("Полёт окончен по плану. Время полёта: {0} секунд. Дистанция: {1} метров", flt, cXYT[0].Last());
+            Console.WriteLine("Полёт окончен по плану. Время полёта: {0} секунд. Дистанция: {1} метров", cXYT[2].Last(), cXYT[0].Last());
             f.Close();
+        }
+        public double WindX(double arg)
+        {
+
+            return Math.Sin(arg) * 0.1; 
+        }
+        public double WindY(double arg)
+        {
+            return Math.Cos(arg)*0.1;
         }
     }
 
@@ -65,7 +82,7 @@ namespace Movement
 
         static void Main(string[] args)
         {
-            double sp, ang, step;
+            double sp, ang, step,m;
             sp = Convert.ToDouble(Console.ReadLine());
             ang = Convert.ToDouble(Console.ReadLine());
             step = Convert.ToDouble(Console.ReadLine());
